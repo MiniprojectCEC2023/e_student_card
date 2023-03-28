3f71e548aa40010e4bd83c08a300ca0750ff5daa
Shiban.M <shibanm23@gmail.com>
2023-03-28 22:35:26 +0530

    view and delete user

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request, redirect, session, flash
 import logging
 from flask_mysqldb import MySQL
 
+app = Flask(__name__)
 
+app.secret_key = 'your_secret_key_here'
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 
@@ -15,10 +17,14 @@ app.config['MYSQL_DB'] = 'mini'
 mysql = MySQL(app)
 
 
-# Define routes
+# Define 
+
+
+#INDEX_ROUTE
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/admin-login', methods=['GET', 'POST'])
 def admin_login():
@@ -36,9 +42,13 @@ def admin_login():
     else:
         return render_template('admin-login.html')
 
+
+
 @app.route('/register')
 def registers():
     return render_template('register.html')
+
+
 @app.route("/register-success", methods=["POST"])
 def register():
     if request.method == 'POST':
@@ -79,9 +89,13 @@ def register():
     else:
         return render_template('register.html')
 
+
+
 @app.route("/register-success")
 def success():
     return "Registration successful!"
+
+
 
 @app.route('/librarian-login', methods=['GET', 'POST'])
 def librarian_login():
@@ -99,6 +113,8 @@ def librarian_login():
     else:
         return render_template('librarian-login.html')
 
+
+
 @app.route('/college-office-login', methods=['GET', 'POST'])
 def college_office_login():
     if request.method == 'POST':
@@ -115,6 +131,8 @@ def college_office_login():
     else:
         return render_template('college-office-login.html')
 
+
+
 @app.route('/admin-dashboard')
 def admin_dashboard():
     if session.get('username') == 'admin':
@@ -124,6 +142,8 @@ def admin_dashboard():
         flash(error)
         logging.warning(error)
         return redirect('/admin-login')
+
+
 
 @app.route('/librarian-dashboard')
 def librarian_dashboard():
@@ -135,6 +155,8 @@ def librarian_dashboard():
         logging.warning(error)
         return redirect('/librarian-login')
 
+
+
 @app.route('/college-office-dashboard')
 def college_office_dashboard():
     if session.get('username') == 'office':
@@ -144,11 +166,50 @@ def college_office_dashboard():
         flash(error)
         logging.warning(error)
         return redirect('/college-office-login')
+    
+
+
+@app.route('/view-students')
+def view_students():
+    if session.get('username') == 'admin':
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT name, email, register_number FROM student')
+        students = cur.fetchall()
+        cur.close()
+        return render_template('view-students.html', students=students)
+    else:
+        error = 'You need to log in as admin first.'
+        flash(error)
+        logging.warning(error)
+        return redirect('/admin-login')
+    
+
+
+@app.route('/delete-student/<register_number>', methods=['GET'])
+def delete_student(register_number):
+    if session.get('username') == 'admin':
+        cur = mysql.connection.cursor()
+        cur.execute('DELETE FROM student WHERE register_number = %s', (register_number,))
+        mysql.connection.commit()
+        cur.close()
+        flash('Student record deleted successfully')
+        return redirect('/view-students')
+    else:
+        error = 'You need to log in as admin first.'
+        flash(error)
+        logging.warning(error)
+        return redirect('/admin-login')
+
+
+
+
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect('/')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
